a3e1b52ffbad225468010e72a2a8b31934067ba6
elisa <bascongarcia@gmail.com>
2022-05-27 14:28:10 +0200

    all done less iteration 7

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return pos
+    }
+  }
 
-  max() {}
+  max() { 
+    if (this.items.length === 0) { 
+      throw new Error("EmptySortedList"); 
+    } else {
+      return Math.max(...this.items); 
+    } 
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce((a, b) => a + b, 0);
+    return sum;
+  }
 
-  avg() {}
+  // esta no me sale :_(
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      const average = sum / this.length;
+      return average;
+    }
+  }
 }
 
 module.exports = SortedList;
